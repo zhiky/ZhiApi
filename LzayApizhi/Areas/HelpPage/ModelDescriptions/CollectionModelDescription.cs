9ca7ac4b2e3b91ceaac62071685a9308d3dc6746
74423 <74423@DESKTOP-VT335RP>
2020-04-23 09:02:15 +0800

    添加项目文件。

---
+++ LzayApizhi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace LzayApizhi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
